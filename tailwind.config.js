80b24d575cf7db9473c3cdc67d84c6ed10cb7260
tyago343 <tyagocasanova@gmail.com>
2022-03-14 00:03:17 +0100

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        isabelline: '#f7f3eb',
+        'quick-silver': '#A0A4AA',
+        'dark-puce': '#4a4542',
+      },
+    },
   },
   plugins: [],
 }
