c497c0c7eebe92d9b0d3be518f45ebf3bffaebe6
tyago343 <tyagocasanova@gmail.com>
2022-06-13 02:41:14 +0200

    reestarting proyect with react

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+const Login = () => {
+  return (
+    <section>
+      <div>
+        <form>
+          <p></p>
+          <input type="text" name="username" id="username" />
+          <input type="password" name="password" id="password" />
+          <button>Send</button>
+        </form>
+      </div>
+    </section>
+  );
+};
+export default Login;
---
+++ src/styles/colors.ts
@@ -0,0 +1,1 @@
+export const primary = "#41362f";
