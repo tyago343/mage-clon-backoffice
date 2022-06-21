f1498ec75d1b713a11b7a7160778dc812c0118d0
tyago343 <tyagocasanova@gmail.com>
2022-06-21 18:01:19 +0200

    Added structure in home dashboard and changing header to menu

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,15 @@
-import Header from "@Components/Header";
 import React from "react";
+import Menu from "@Components/Menu";
+import styles from './index.module.scss'
 const Home = () => {
-  return <div><Header />You are in bitches</div>;
+  return (
+    <main>
+      <Menu />
+      <div className={styles.pageWrapper}>Esto tiene que funcionar</div>
+      <header></header>
+      <main></main>
+      <footer></footer>
+    </main>
+  );
 };
 export default Home;
