15482557b96cab3cf8115ba03b52bb1c9ebb2327
tyago343 <tyagocasanova@gmail.com>
2022-06-27 01:23:17 +0200

    styles in menu

--- src/components/molecules/Menu/index.tsx
+++ src/components/molecules/Menu/index.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { FcShop, FcFactory } from "react-icons/fc";
+import { AiOutlineClose } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import DashboardRoutes from "src/routes/Dashboard";
 import styles from "./index.module.scss";
-const Header = () => {
+const Menu = () => {
+  const [activeItem, setActiveItem] = useState("");
   return (
     <aside className={styles.menuWrapper}>
       <Link to={DashboardRoutes.INDEX} className={styles.logo}>
@@ -13,11 +15,20 @@ const Header = () => {
         <ul>
           <li>
             <div className={styles.menuItem}>
-              <FcFactory size={50} />
-              <span>Catalog</span>
-              <div>
-                <h2>Catalog</h2>
-                <ul>
+              <p onClick={() => setActiveItem("catalog")} className={styles.menuItemTitle}>
+                <FcFactory size={50} />
+                <span>Catalog</span>
+              </p>
+              <div
+                className={`${styles.menuItemWrapper} ${
+                  activeItem === "catalog" ? styles.active : ""
+                }`}
+              >
+                <strong className={styles.menuItemSubtitle}>Catalog</strong>
+                <span className={styles.menuItemClose}>
+                  <AiOutlineClose size={20} />
+                </span>
+                <ul className={styles.menuItemList}>
                   <li>
                     <Link to={DashboardRoutes.PRODUCTS}>Products</Link>
                   </li>
@@ -30,4 +41,4 @@ const Header = () => {
     </aside>
   );
 };
-export default Header;
+export default Menu;
